e5a9c4fcf1666138dc67b3e45d240513a8d92658
Simon Aumayer <simon.aumayer@aboutlabs.de>
2024-09-15 20:29:28 +0200

    Revert "Revert "reintroduced spa layout""
    This reverts commit 0b6a56dc083d58c40a953d1314204fe3f641e53b.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,24 @@
 # main.py
 
 import streamlit as st
+import os
 from utils.report_generation import generate_report
+from utils.helper import load_report
 
-# Safely retrieve secrets from secrets.toml or Streamlit Cloud's secret manager
-alpha_vantage_api_key = st.secrets["ALPHA_VANTAGE_API_KEY"]
+# Set page configuration
+st.set_page_config(page_title="Investment Analysis Report Generator")
 
-# Streamlit interface
+# Display the logo
+logo_path = "assets/dtb-logo.jpg"
+st.image(logo_path, width=128)
+
+# Title
 st.title("Investment Analysis Report Generator")
 
+# Input for stock symbol
 stock_symbol = st.text_input("Enter stock symbol:", "AAPL")
 
+# Generate Report Section
 if st.button("Generate Report"):
     report_file = generate_report(stock_symbol)
     if report_file:
@@ -19,3 +27,40 @@ if st.button("Generate Report"):
             st.markdown(f.read())
     else:
         st.error("Failed to generate report.")
+
+# Report Management Section
+st.write("### Available Reports")
+
+# List all .md reports in the reports directory
+reports = [report for report in os.listdir('reports') if report.endswith('.md')]
+selected_report = st.selectbox("Select a report to view or delete", reports)
+
+if selected_report:
+    report_base_name = os.path.splitext(selected_report)[0]
+    image_file = f'reports/{report_base_name}.png'
+    
+    if st.button("Delete Selected Report"):
+        # Check if the report file exists before attempting to delete
+        if os.path.exists(f'reports/{selected_report}'):
+            os.remove(f'reports/{selected_report}')
+            
+            # Delete the corresponding image
+            if os.path.exists(image_file):
+                os.remove(image_file)
+            
+            st.success(f"Report {selected_report} and its associated image deleted successfully!")
+        else:
+            st.error(f"Report {selected_report} not found. It may have already been deleted.")
+
+    # Load and display the selected report
+    report_content, image_file = load_report(selected_report)
+    if report_content:
+        st.markdown(report_content)
+
+        # Display the corresponding image if it exists
+        if os.path.exists(image_file):
+            st.image(image_file, caption="Candlestick Chart")
+        else:
+            st.write("No associated image found.")
+    else:
+        st.error("Failed to load the report.")
